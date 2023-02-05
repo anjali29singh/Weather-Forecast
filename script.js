9fb9fd66ede54d15b34b36355a0aec3154060ea9
Anjali <anjalisingh930260@gmail.com>
2023-02-05 18:36:58 +0530

    second commit

--- script.js
+++ script.js
@@ -1,118 +1,163 @@
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
 const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
 const locationBlock = document.querySelector('.location-block');
 const input = document.querySelector('.search-box');
 const submitBtn = document.querySelector('.submit');
 const geoLocation = document.querySelector('.device-location');
+const forecastCont=document.querySelector('.forecast-cont');
+const cityName = document.querySelector('.city-name');
 const region = document.querySelector('.region');
 const country = document.querySelector('.country');
 const weatherImg = document.querySelector('.weather-icon-img')
 const date = document.querySelector('.date');
 const tempInCenti = document.querySelector('.tempt-in-centi');
+const tempInFeh = document.querySelector('.tempt-in-feh');
 const weatherCondition = document.querySelector('.condition');
 const humidityVal = document.querySelector('.humid-val');
 const windVal = document.querySelector('.wind-val');
 const preciptVal = document.querySelector('.prep-val');
 
+
+//display forecast
+
+function displayForecast(){
+    forecastCont.classList.remove('hidden');
+
+}
 //Update Region
-function updateRegion(rgn, cont) {
+function updateRegion(name, rgn, cont) {
+    cityName.textContent = name;
     region.textContent = rgn;
     country.textContent = cont;
 
 
 }
-//fetching and getting response
-async function getData(url) {
-    const resp = await fetch(url,{
-    });
-    return resp.json();
+//update tempt
+function updateTempt(temp_c, temp_f, condition) {
+    
+    tempInCenti.textContent = temp_c;
+    tempInFeh.textContent = temp_f;
+    weatherCondition.textContent = condition;
+    
+    
+    
 }
-//Update Weather 
-function updateWeather(weatherData) {
-    const { wind_spd, rh, weather, precip, temp } = weatherData.data[0];
-    const weatherSituation = weather.description;
 
-    console.log(weatherData);
+//updating date and time
+function updateDateTime() {
     d = new Date();
     const dt = `${d.getDate()} ${months[d.getMonth()]} ${d.getFullYear()},${days[d.getDay()]}`
 
     date.textContent = dt;
 
-    tempInCenti.textContent = temp;
-
-    weatherCondition.textContent = weatherSituation;
-
-    humidityVal.textContent = `${rh} %`;
-
-    windVal.textContent = `${wind_spd.toFixed(3)} m/sec`;
-
-    if (!precip) preciptVal.textContent = `${0} mm/hr`;
-    else {
-        preciptVal.textContent = `${precip} mm/hr`
-    };
-
-
-
 }
+
+
 //Insert Weather icon 
 function addIcon(icon) {
-
-    weatherImg.src = `https://www.weatherbit.io/static/img/icons/${icon}.png`;
+    
+    weatherImg.src = icon;
 }
+
 //Display Error
 function renderError(err) {
+    alert(err);
+    
+}
+//Update Weather 
+function WeatherDetails(hum, wind, precip) {
+
+    humidityVal.textContent = `${hum} %`;
+
+    windVal.textContent = `${wind.toFixed(2)} km/hr`;
+
+
+    preciptVal.textContent = `${precip} mm/hr`
 
 
 }
 
-//hide search box 
-function hideLocationBlock() {
-    locationBlock.classList.add('hidden');
+//updating function
+function updatingFunc(data){
+
+
+    const { name, region, country } = data.location;
+            const { temp_c, temp_f, wind_kph, precip_mm, humidity } = data.current;
+            const { text, icon, code } = data.current.condition;
+
+
+            updateRegion(name, region, country);
+
+            updateDateTime();
+
+            updateTempt(temp_c, temp_f, text);
+
+            addIcon(icon);
+
+            WeatherDetails(humidity, wind_kph, precip_mm);
+            
+            displayForecast();
+
+
+
 }
+
+//fetching and getting response
+async function getData(url) {
+    const resp = await fetch(url, {
+    });
+    if (!resp) return new Error ('Something went wrong');
+    return resp.json();
+}
+
+
 
 //Getting weather forecast
-function WeatherDetails(city) {
-
-    // getData(`https://api.weatherbit.io/v2.0/current?city=${city}&key=fb180e6dc648403fa5c80e00e284df5c&include=minutely`)
+function getForecastBycity(city) {
 
     getData(`http://api.weatherapi.com/v1/current.json?key=49eda03a1d524774b5962019230502&q=${city}&aqi=no`)
         .then(resp => {
 
-            console.log(resp);
+            updatingFunc(resp);
 
-            // const { icon } = resp.data[0].weather;
-            // const {lat,lon}=resp.data[0];
-        
-
-            //get state and country
-            getPlace(lat,lon);
-            
-            //inserting icon in html
-            addIcon(icon);
-
-            //Update date ,time,tempt etc
-            updateWeather(resp);
-
-            
 
 
         })
         .catch(err => {
-            renderError(err);
+            renderError(`Something went wrong`);
         })
 
 }
+function getForecastByCoords(lat,lng){
+getData(`http://api.weatherapi.com/v1/current.json?key=49eda03a1d524774b5962019230502&q=${lat},${lng}&aqi=no`)
+        .then(data => {
+            updatingFunc(data);
 
+        })
+        .catch(err => {
+            renderError(`Something went wrong `);
+        });
+
+
+}
 submitBtn.addEventListener('click', function (e) {
     e.preventDefault();
     const city = input.value;
+    if (!city){
+        renderError('Please enter valid city');
+    }
+    else{
+
 
     // requestApi(city);
-    WeatherDetails(city);
-
-
+    getForecastBycity(city);
+    
+    
     //clear inputs
     input.value = '';
+    }
 
 });
 
@@ -124,36 +169,11 @@ geoLocation.addEventListener('click', function (e) {
     }
 });
 
-//get city state and country
-function getPlace(lat, lng) {
-    // getData(`https://geocode.xyz/${lat},${lng}?json=1&auth=981110903134814621444x98407`
-    getData(`http://api.weatherapi.com/v1/current.json?key=49eda03a1d524774b5962019230502&q=${lat}/${lng}&aqi=no`)
-    .then(data => {
-        console.log(data);
-        const { city, country, region } = data;
-
-        WeatherDetails(city);
-
-        updateRegion(region, country);
-
-
-    })
-        .catch(err => {
-            renderError(err);
-        });
-
-}
-
-
-//get city
 function success(pos) {
     const { latitude: lat, longitude: lng } = pos.coords;
+    
+    getForecastByCoords(lat,lng);
+       
 
-   getPlace(lat,lng);
+
 }
-
-
-// fetch('http://api.weatherapi.com/v1/current.json?key=49eda03a1d524774b5962019230502&q=bhopal&aqi=no').then(res=>
-// res.json()).then(data=>{
-//     console.log(data);
-// })
